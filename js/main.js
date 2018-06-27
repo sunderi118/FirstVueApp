1732b517c4a97473145b9abd8a7a03b6bb757521
Airi Higa <airi.higa118@gmail.com>
2018-06-27 18:48:11 +0900

    first vue app

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// (function () {
+//     'use strict';
+//
+//     var vm = new Vue({
+//         el:"#app",
+//         data: {
+//             todos:[
+//                 'task1',
+//                 'task2',
+//                 'task3'
+//             ]
+//         }
+//     });
+// })();
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
